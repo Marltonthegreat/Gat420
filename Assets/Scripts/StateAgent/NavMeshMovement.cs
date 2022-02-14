2a3f75edcc3409cdf42a43fbadaa911b822ca552
Marltonthegreat <75112858+Marltonthegreat@users.noreply.github.com>
2022-02-14 11:58:34 -0700

    2/14/22 UtilityAgent Creation

--- Assets/Scripts/StateAgent/NavMeshMovement.cs
+++ Assets/Scripts/StateAgent/NavMeshMovement.cs
@@ -6,17 +6,12 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class NavMeshMovement : Movement
 {
+    public NavMeshAgent navMeshAgent;
+
     public override Vector3 velocity
     {
         get => navMeshAgent.velocity;
         set => navMeshAgent.velocity = value;
-    }
-
-    NavMeshAgent navMeshAgent;
-
-    void Start()
-    {
-        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
